c27ff21d02fae8b044a22c8d874dfbd03e4eff75
manaparvar <manaabbasparvar@gmail.com>
2025-11-18 17:57:13 -0800

    addingHeaderFooter

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Content from "./components/Content";
 
 export default function Home() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Content />
+      <Footer />
+    </>
+  );
 }
